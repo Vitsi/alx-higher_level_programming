77bf24250bbadb69147173a2a7e399d8446b779c
Vitsi <lessons019@gmail.com>
2023-12-07 04:43:13 -0800

    task

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# BY Tsion
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# BY Tsion
+
+def pow(a, b):
+    return (a ** b)
