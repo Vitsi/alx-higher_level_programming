ac2395b294d8954f019908499a5145871e6a8fab
Vitsi <lessons019@gmail.com>
2024-09-09 23:11:48 -0700

    fixing task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
-import sys
+from sys import argv
+"""access commandline arguments"""
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""create object from JSON file"""
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+"""writes an object to text file, using JSON representation"""
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+filename = "add_item.json"
+try:
+    content = load_from_json_file(filename)
+except:
+    content = []
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    
-    items.extend(sys.argv[1:])
-    
-    # Save updated list to the file
-    save_to_json_file(items, "add_item.json")
+for i in range(1, len(argv)):
+    content.append(argv[i])
+save_to_json_file(content, filename)
